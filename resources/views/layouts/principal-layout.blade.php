e405bf827b9232cf7529b98278f16c5a209df5a0
pirespsps <matheusknoll28@gmail.com>
2025-08-16 14:14:34 -0300

    initial push

---
+++ resources/views/layouts/principal-layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/scss/app.scss','resources/js/app.js'])
+    <link rel="stylesheet" href="style.css">
+    <title>Site Disco</title>
+</head>
+<body>
+<h1>Layout</h1>
+
+@yield('content')
+
+
+@yield('script')
+
+</body>
+</html>
