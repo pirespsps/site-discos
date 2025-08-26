cd1e18a39deab21bc8766d3174941e626f526728
pirespsps <matheusknoll28@gmail.com>
2025-08-25 21:36:37 -0300

    Criação de arquivos e rotas

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller{
+
+    public function cadastro(Request $request){
+
+        return view("cadastro");
+    }
+    public function cadastroEntrar(Request $request){
+
+    }
+
+    public function login(Request $request){
+
+        return view("login");
+    }
+
+    
+    public function loginEntrar(Request $request){
+
+    }
+
+    
+    public function logout(Request $request){
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IndexController;
 
-Route::get('/siteDisco', function () {
-    return view('');
-});
+
+Route::get('/', [IndexController::class,"index"]);
+
+Route::get("/cadastro",[AuthController::class,"cadastro"]);
+Route::post("/cadastro/entrar",[AuthController::class,"cadastroEntrar"])->name("cadastro-entrar");
+
+Route::get('/login',[AuthController::class,"login"]);
+Route::post("/login/entrar",[AuthController::class,"loginEntrar"])->name("login-entrar");
+
+Route::post("/logout",[AuthController::class,"logout"]);
