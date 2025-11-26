20b3ffeadd6f03080dc974e4c05e7d40a30be6c0
Lucas <lucasdiasmaschio@gmail.com>
2025-11-26 19:43:22 -0300

    tentatica de deixar a lista certa

--- resources/views/layouts/list.blade.php
+++ resources/views/layouts/list.blade.php
@@ -3,17 +3,18 @@
 @yield('body')
 <div class="container w-100 h-100 d-flex">
 
-    <div class="bg-dark d-block column">
+    <div class="bg-dark d-block w-100 column p-3">
         @foreach ($multipleData as [$cover, $name, $value, $id])
 
-            <div class="d-inline">
-                <div class="d-flex">
-                    <img src="{{ asset('storage/' . $cover) }}" class="h-25 w-25">
-                <a href="{{ ($type == "disco" ? "discos" : $type == "banda") ? "bandas" : "musicas" }}/{{ $id }}" class="text-decoration-none text-white">
+            <div class="bg-dark w-75 h-25 rounded mh-5 mt-2 p-3 d-flex border border-primary">
+                    <div class = "d-block w-25 h-50">
+                        <img src="{{ asset('storage/' . $cover) }}" class="h-100 w-100 justify-content-center bg">
+                    </div>
+                <a href="{{ ($type == "disco" ? "discos" : $type == "banda") ? "bandas" : "musicas" }}/{{ $id }}" class="text-decoration-none text-white p-3 h-25">
                     <h3>{{ $name }}</h3>
                 </a>
-                <h3 class="text-default">{{ $value }}</h3>
-                </div>
+                <h3 class="text-default pt-3 h-25" >{{ $value }}</h3>
+                
                 
             </div>
 
